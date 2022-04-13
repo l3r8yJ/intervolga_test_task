3f8ecf06eac6437c971ec243c979929f6cd437ef
Ivan <clicker.heroes.acg@gmail.com>
2022-04-13 10:34:23 +0300

    big refactor

--- src/controllers/controller.php
+++ src/controllers/controller.php
@@ -9,35 +9,4 @@ class Controller
     {
         $this->model = $model;
     }
-
-    public function getAllStudents()
-    {
-        return $this->model->getStudentsFromDatabase();
-    }
-
-    public function getById($id)
-    {
-        return $this->model->getStudentById($id);
-    }
-
-    public function create($name, $surname, $birthday, $photo)
-    {
-        if (isset($_POST)) {
-            $this->model->createStudent($name, $surname, $birthday, $photo);
-            header("Location: http://localhost/index.php");
-        } else {
-            require $_SERVER['DOCUMENT_ROOT'] . '/src/views/form.php';
-        }
-    }
-
-    public function edit($id, $name, $surname, $birthday, $photo)
-    {
-        if (isset($_POST)) {
-            $this->model->updateStudentById($id, $name, $surname, $birthday, $photo);
-            header("Location: http://localhost/index.php");
-        } else {
-            require $_SERVER['DOCUMENT_ROOT'] . '/src/views/form.php';
-        }
-
-    }
 }
