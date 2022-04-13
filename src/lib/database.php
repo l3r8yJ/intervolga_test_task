61ad22fd6fee25aee61bee304cc967128b3cd9c6
Ivan <clicker.heroes.acg@gmail.com>
2022-04-13 10:47:32 +0300

    database class via redBean ORM

--- src/lib/database.php
+++ src/lib/database.php
@@ -1,25 +1,17 @@
 <?php
+require '../lib/rb.php';
+
 class Database
 {
     private $userName = 'root', $password = 'root', $host = '127.0.0.1', $port = '8889', $name;
 
-    // @param String $name
-    public function __construct($name)
+    public function openConnection()
     {
-        $this->name = $name;
+        R::setup("mysql:host=$this->host;port=$this->port;dbname=$this->name", $this->userName, $this->password);
     }
 
-    // @return PDO
-    public function getConnection()
-    {
-        return new PDO("mysql:host=$this->host;port=$this->port;dbname=$this->name", $this->userName, $this->password);
-    }
-
-    // idk what would be better, create a @param PDO $connection
-    // or
-    // make this method close self connection, - in current version this variant.
     public function closeConnection()
     {
-        $this->connection = null;
+        R::close();
     }
 }
