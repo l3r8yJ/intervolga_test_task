3fa6991dc724899e17c807eb73bd901121a3f0b8
Ivan Ivanchuk <clicker.heroes.acg@gmail.com>
2022-04-14 13:16:51 +0300

    upd

--- src/views/list.php
+++ src/views/list.php
@@ -2,36 +2,39 @@
 <div class="table-wrapper">
     <table class="fl-table">
         <tr>
-        <th>id</th>
-        <th>Name</th>
-        <th>Surname</th>
-        <th>Birthday</th>
+            <th>id</th>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Birthday</th>
         </tr>
+        
         <?php foreach ($list as $student): ?>
-        <tr>
-        <th><?=htmlspecialchars($student['id']);?></th>
-        <th><?=htmlspecialchars($student['name']);?></th>
-        <th><?=htmlspecialchars($student['surname']);?></th>
-        <th><?=htmlspecialchars($student['birthday']);?></th>
+            <tr>
+                <th><?=htmlspecialchars($student['id']);?></th>
+                <th><?=htmlspecialchars($student['name']);?></th>
+                <th><?=htmlspecialchars($student['surname']);?></th>
+                <th><?=htmlspecialchars($student['birthday']);?></th>
 
-        <th style="width: 10%;">
-            <a href="<?=$_SERVER['HTTP_REQUEST'] . '/src/views/'?>edit/edit.php?id=<?=$student['id']?>">
-                <img src="<?=$_SERVER['HTTP_REQUEST']?>/public/images/edit.png" alt="" style="width: 30%;">
-            </a>
-        </th>
+                <th style="width: 10%;">
+                    <a href="<?=$_SERVER['HTTP_REQUEST'] . '/src/views/'?>edit/edit.php?id=<?=$student['id']?>">
+                        <img src="<?=$_SERVER['HTTP_REQUEST']?>/public/images/edit.png" alt="" style="width: 30%;">
+                    </a>
+                </th>
 
-        <th style="width: 10%;">
-            <a href="<?=$_SERVER['HTTP_REQUEST'] . '/src/views/'?>delete/delete.php?id=<?=$student['id']?>">
-                <img src="<?=$_SERVER['HTTP_REQUEST']?>/public/images/delete.png" alt="" style="width: 30%;">
-            </a>
-        </th>
-        </tr>
+                <th style="width: 10%;">
+                    <a href="<?=$_SERVER['HTTP_REQUEST'] . '/src/views/'?>delete/delete.php?id=<?=$student['id']?>">
+                        <img src="<?=$_SERVER['HTTP_REQUEST']?>/public/images/delete.png" alt="" style="width: 30%;">
+                    </a>
+                </th>
+            </tr>
         <?php endforeach;?>
+        
         <th width="200">
             <a href="<?=$_SERVER['HTTP_REQUEST']?>/src/views/create/create.php">
                 <button>Add new</button>
             </a>
         </th>
+    
     </table>
 </div>
 <?php
