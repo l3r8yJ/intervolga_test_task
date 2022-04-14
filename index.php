d4ab2324c7cdb9954d32f0d2eb40399a6ec3dd14
Ivan <clicker.heroes.acg@gmail.com>
2022-04-14 14:01:16 +0300

    back

--- index.php
+++ index.php
@@ -2,15 +2,11 @@
 
 require $_SERVER['DOCUMENT_ROOT'] . '/src/controllers/controller.php';
 
-try {
-    $controller = new Controller(
-        new StudentModel(
-            new Database('test_task', 'root', 'root', '127.0.0.1', '8889')
-        )
-    );
-} catch (Exception $e) {
-    echo 'Error: ' . $e->getMessage();
-}
+$controller = new Controller(
+    new StudentModel(
+        new Database('test_task', 'root', 'root', '127.0.0.1', '8889')
+    )
+);
 
 $request = str_replace('.php', '', basename($_SERVER['PHP_SELF']));
 $request = trim($request, '/');
