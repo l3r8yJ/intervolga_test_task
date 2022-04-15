b080280b4bf888960e59c15b70caacd84eb9e139
Ivan <clicker.heroes.acg@gmail.com>
2022-04-15 11:06:15 +0300

    styles upload added

--- src/views/add/add_photo.php
+++ src/views/add/add_photo.php
@@ -8,15 +8,19 @@ include $_SERVER['DOCUMENT_ROOT'] . '/index.php';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../../public/styles/css/form.css">
+  <link rel="stylesheet" href="<?=$_SERVER['HTTP_REQUEST']?>/public/styles/css/photo_form.css">
   <title>Add photo</title>
 </head>
 <body>
 
   <form method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="photo" id="photo">
-    <input type="submit" value="Upload Image" name="submit">
+    <div class="wrapper">
+      <div class="file-upload">
+        <input type="file" name="photo" id="photo">
+        <i class="fa fa-arrow-up"></i>
+      </div>
+    </div>
+    <input type="submit" class="submit" value="Upload Image" name="submit">
   </form>
 
 </body>
